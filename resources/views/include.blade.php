ba6f3c7d249d60dade3490db21702343b2befb84
SebatZaman007 <tasnimzamansebat@gmail.com>
2021-11-23 12:29:07 +0600

    again commit

--- resources/views/include.blade.php
+++ resources/views/include.blade.php
@@ -12,10 +12,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{url('/dashboard')}}">Dashboard</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('about.page')}}">About</a>
-                </li>
-
             </ul>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
